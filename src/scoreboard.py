e26a4f623756d8cc5e5591296a6b6b5773e9f6cb
Richard Rossi <richard.rossi15@gmail.com>
2019-08-12 21:16:11 -0400

    Colors have moved

--- src/scoreboard.py
+++ src/scoreboard.py
@@ -1,3 +1,5 @@
+from ui_utils import Colors
+
 import board
 import ui_utils
 import pygame
@@ -38,56 +40,56 @@ class ScoreboardUI():
 
         # Background for the scoreboard
         boardRect = pygame.Rect([self.pos_x, self.pos_y, self.width, row_height*5+divider_width])
-        pygame.draw.rect(screen, ui_utils.PURPLE, boardRect)
+        pygame.draw.rect(screen, Colors.PURPLE, boardRect)
 
         #Column dividers
         for col in range(6):
             x = self.pos_x + col*col_width
             y = self.pos_y
-            pygame.draw.rect(screen, ui_utils.WHITE, [x, y, divider_width, self.height])
-            pygame.draw.rect(screen, ui_utils.WHITE, [x, y, divider_width, self.height])
+            pygame.draw.rect(screen, Colors.WHITE, [x, y, divider_width, self.height])
+            pygame.draw.rect(screen, Colors.WHITE, [x, y, divider_width, self.height])
 
         #Column headers
-        text = font.render('Round 1', True, ui_utils.WHITE)
+        text = font.render('Round 1', True, Colors.WHITE)
         screen.blit(text, [col1_start+col_width+xtext_offset, row1_start+ytext_offset])
-        text = font.render('Round 2', True, ui_utils.WHITE)
+        text = font.render('Round 2', True, Colors.WHITE)
         screen.blit(text, [col1_start+2*col_width+xtext_offset, row1_start+ytext_offset])
-        text = font.render('Total', True, ui_utils.WHITE)
+        text = font.render('Total', True, Colors.WHITE)
         screen.blit(text, [col1_start+3*col_width+xtext_offset, row1_start+ytext_offset])
-        text = font.render('Spin Tokens', True, ui_utils.WHITE)
+        text = font.render('Spin Tokens', True, Colors.WHITE)
         screen.blit(text, [col1_start+4*col_width+xtext_offset, row1_start+ytext_offset])
 
         #Row dividers
         for row in range(6):
             x = col1_start
             y = row1_start + row_height*row
-            pygame.draw.rect(screen, ui_utils.WHITE, [x, y, self.width, divider_width])
+            pygame.draw.rect(screen, Colors.WHITE, [x, y, self.width, divider_width])
 
         for i in range(self.app.num_players):
             y = row1_start + (i+1)*row_height + ytext_offset
 
             player_text = 'Player %i' % (i+1)
             x = col1_start + xtext_offset
-            text = font.render(player_text, True, ui_utils.WHITE)
+            text = font.render(player_text, True, Colors.WHITE)
             screen.blit(text, [x, y])
 
             round1_text = '$%i' % self.app.players[i].playerRoundScore[0]
-            text = font.render(round1_text, True, ui_utils.WHITE)
+            text = font.render(round1_text, True, Colors.WHITE)
             x += col_width
             screen.blit(text, [x, y])
 
             round2_text = '$%i' % self.app.players[i].playerRoundScore[1]
-            text = font.render(round2_text, True, ui_utils.WHITE)
+            text = font.render(round2_text, True, Colors.WHITE)
             x += col_width
             screen.blit(text, [x, y])
 
             total_text = '$%i' % (self.app.players[i].finalScore())
-            text = font.render(total_text, True, ui_utils.WHITE)
+            text = font.render(total_text, True, Colors.WHITE)
             x += col_width
             screen.blit(text, [x, y])
 
             tokens_text = '%i' % self.app.players[i].playerTokenCount
-            text = font.render(tokens_text, True, ui_utils.WHITE)
+            text = font.render(tokens_text, True, Colors.WHITE)
             x += col_width
             screen.blit(text, [x, y])
 
@@ -95,33 +97,33 @@ class ScoreboardUI():
             x += col_width
 
         if self.app.game_over:
-            text = font.render('GAME OVER', True, ui_utils.WHITE)
+            text = font.render('GAME OVER', True, Colors.WHITE)
             screen.blit(text, [x,y+50])
 
             if self.checkTie():
-                text = font.render('Tie Game', True, ui_utils.WHITE)
+                text = font.render('Tie Game', True, Colors.WHITE)
                 screen.blit(text, [x,y+100])
             else:
-                text = font.render('Winner:', True, ui_utils.WHITE)
+                text = font.render('Winner:', True, Colors.WHITE)
                 screen.blit(text, [x,y+100])
 
-                text = font.render('Player %i' % (self.findWinner()), True, ui_utils.WHITE)
+                text = font.render('Player %i' % (self.findWinner()), True, Colors.WHITE)
                 screen.blit(text, [x+50,y+125])
 
         else:
-            text = font.render('Current Player', True, ui_utils.WHITE)
+            text = font.render('Current Player', True, Colors.WHITE)
             screen.blit(text, [x,y])
-            text = font.render('%i' % (self.app.cur_player_index+1), True, ui_utils.WHITE)
+            text = font.render('%i' % (self.app.cur_player_index+1), True, Colors.WHITE)
             screen.blit(text, [x+50,y+15])
 
-            text = font.render('Current Round', True, ui_utils.WHITE)
+            text = font.render('Current Round', True, Colors.WHITE)
             screen.blit(text, [x,y+50])
-            text = font.render('%i' % (self.app.cur_round), True, ui_utils.WHITE)
+            text = font.render('%i' % (self.app.cur_round), True, Colors.WHITE)
             screen.blit(text, [x+50,y+65])
 
-            text = font.render('Spins Remainig', True, ui_utils.WHITE)
+            text = font.render('Spins Remainig', True, Colors.WHITE)
             screen.blit(text, [x,y+100])
-            text = font.render('%i' % (self.app.spinsRemaining), True, ui_utils.WHITE)
+            text = font.render('%i' % (self.app.spinsRemaining), True, Colors.WHITE)
             screen.blit(text, [x+50,y+115])
 
     def checkTie(self):
@@ -132,7 +134,6 @@ class ScoreboardUI():
                 tie = True
             if self.app.players[i].finalScore() > max:
                 max = self.app.players[i].finalScore()
-                winner = i
                 tie = False
         return tie
 
